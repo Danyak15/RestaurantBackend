99efee93ed64b42ae85c5476f11db2186cee884d
Danyak15__ <danyak15@yandex.ru>
2026-07-23 23:20:25 +0300

    Fix: Configure backend environment and test database

--- src/test/kotlin/org/example/restaurantbackend/RestaurantBackendApplicationTests.kt
+++ src/test/kotlin/org/example/restaurantbackend/RestaurantBackendApplicationTests.kt
@@ -2,12 +2,13 @@ package org.example.restaurantbackend
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RestaurantBackendApplicationTests {
 
     @Test
     fun contextLoads() {
     }
-
 }
